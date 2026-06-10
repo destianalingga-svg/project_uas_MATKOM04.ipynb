9c317182ce93fbcf7118592bc5cc42a8792b5299
destianalingga-svg <destianalingga@gmail.com>
2026-06-10 12:15:38 +0700

    Update paket_komplit.app.py

--- paket_komplit.app.py
+++ paket_komplit.app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import sympy as sp
 
-# ==========================================
-# 1. KONFIGURASI HALAMAN & JUDUL
-# ==========================================
+
+
 st.set_page_config(page_title="Project UAS Matematika Komputasi", layout="centered")
